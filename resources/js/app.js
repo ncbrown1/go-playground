5922c5642c41142923a94367f325be775149c872
Nick Brown <nickbrown714@gmail.com>
2017-04-13 21:06:55 -0700

    setting up es2015/scss transpilation foundation for front end dev

---
+++ resources/js/app.js
@@ -0,0 +1,3 @@
+let x = "foo";
+
+console.log(x);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: {
+        'js/bundle.js': './resources/js/app.js',
+        'css/main.css': './resources/css/app.scss'
+    },
+    output: {
+        filename: './public/[name]'
+    },
+    devtool: process.env.NODE_ENV !== 'production' ? 'source-map' : false,
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.scss$/,
+                include: /resources\/css/,
+                loaders: ExtractTextPlugin.extract('css-loader!sass-loader')
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: './public/css/main.css',
+            allChunks: true
+        })
+    ]
+};
