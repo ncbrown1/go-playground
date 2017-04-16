f2377ee7c9e1ece6f6955b7cf2877780b2bf81fa
Nick Brown <nickbrown714@gmail.com>
2017-04-16 16:40:12 -0700

    replicating original layout/design

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,19 @@
 
 require('./setup');
 
+function resizeInput() {
+    $(this).attr('size', $(this).val().length);
+}
+
+let share_url = $('#share-url');
+share_url.keyup(resizeInput)
+    // resize on page load
+    .each(resizeInput);
+$('#share').click(() => {
+    share_url.addClass('active');
+    share_url.focus();
+});
+
 const initial_program = `
 package main
 
@@ -22,3 +35,9 @@ CodeMirror(document.getElementById("go-editor"), {
     theme: 'monokai',
     value: initial_program
 });
+
+const sample_output = 'Hello, playground';
+const sample_system = 'Program exited.';
+
+document.querySelector("#go-output .stdout").innerText = sample_output;
+document.querySelector("#go-output .system").innerText = sample_system;
