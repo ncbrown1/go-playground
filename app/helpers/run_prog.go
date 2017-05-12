a3817bcd492138a2027c7667879d5870422c3cdf
Nick Brown <nickbrown714@gmail.com>
2017-05-12 10:29:07 -0700

    compile program first to get compile error output if necessary

--- app/helpers/run_prog.go
+++ app/helpers/run_prog.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+    "os"
     "os/exec"
     "fmt"
     "flag"
+    "syscall"
+    "bytes"
 )
+
+type Result struct {
+    code   int
+    output string
+    error  string
+}
 
 func main() {
     filePtr := flag.String("file", "", "file path")
@@ -14,19 +23,55 @@ func main() {
         fmt.Print("File chosen: ")
         fmt.Println(*filePtr)
 
-        cmd := exec.Command("go", "run", *filePtr)
-        output, err := cmd.Output()
+        pid := os.Getpid()
+        exec_file := fmt.Sprintf("/tmp/prog-%d", pid)
+        compile_file := fmt.Sprintf("%s-compiled.o", exec_file)
+        compiled := run("go", "tool", "compile", "-o", compile_file, *filePtr)
 
-        fmt.Println("stdout:")
-        fmt.Print(string(output[:]))
+        if compiled.code == 0 {
+            fmt.Printf("Successfully compiled to: %s\n", exec_file)
 
-        fmt.Println("\nstderr:")
-        if err == nil {
-            fmt.Println("NULL")
+            run("go", "build", "-o", exec_file, *filePtr)
+            execd := run(exec_file)
+
+            fmt.Println("stdout:")
+            fmt.Println(execd.output)
+
+            fmt.Println("stderr:")
+            fmt.Println(execd.error)
+
+            fmt.Printf("Exit status: %d\n", execd.code)
         } else {
-            fmt.Println(err)
+            fmt.Print(compiled.output)
         }
     } else {
         fmt.Println("YOU MUST CHOOSE A PROPER FILE TO RUN")
+    }
+}
+
+func run(exe string, args ...string) Result {
+    var exit_code int
+    cmd := exec.Command(exe, args...)
+    cmdOutput := &bytes.Buffer{}
+    cmdError := &bytes.Buffer{}
+    cmd.Stdout = cmdOutput
+    cmd.Stderr = cmdError
+
+    if err := cmd.Run(); err != nil {
+        //if err != nil {
+        //    os.Stderr.WriteString(fmt.Sprintf("Error: %s\n", err.Error()))
+        //}
+        if exitError, ok := err.(*exec.ExitError); ok {
+            exit_code = exitError.Sys().(syscall.WaitStatus).ExitStatus()
+        }
+    } else {
+        // Success
+        exit_code = cmd.ProcessState.Sys().(syscall.WaitStatus).ExitStatus()
+    }
+
+    return Result{
+        exit_code,
+        string(cmdOutput.Bytes()),
+        string(cmdError.Bytes()),
     }
 }
