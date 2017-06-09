790c15e87bc63073ae8569a00d50a792664b2eb5
Nick Brown <nickbrown714@gmail.com>
2017-06-09 01:41:14 -0700

    removing redundant Message struct

--- app/WSClient.go
+++ app/WSClient.go
@@ -8,15 +8,6 @@ import (
     "github.com/ncbrown1/go-playground/app/runtime"
 )
 
-// Message is the wire format for the websocket connection to the browser.
-// It is used for both sending output messages and receiving commands, as
-// distinguished by the Kind field.
-type Message struct {
-    Id   string `json:"id"`   // client-provided unique id for the process
-    Kind string `json:"kind"` // in: "run", "kill" out: "stdout", "stderr", "end"
-    Body string `json:"body"`
-}
-
 var wsupgrader = websocket.Upgrader{
     ReadBufferSize:  1024,
     WriteBufferSize: 1024,
